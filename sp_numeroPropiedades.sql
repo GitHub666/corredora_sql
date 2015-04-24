a33445d19827ed8b676baa2694c855c5d81c8578
C. Ruz <c.ruz.olate@gmail.com>
2015-04-24 00:22:18 -0300

    Creacion archivos procedimientos almacenados

---
+++ sp_numeroPropiedades.sql
@@ -0,0 +1,29 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+USE corredora
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Christopher Ruz
+-- Create date: 23-01-2015
+-- Description:	sp_numeroPropiedades permite obtener los propietarios con mas de una
+--				propiedad
+-- =============================================
+CREATE PROCEDURE sp_numeroPropiedades
+	
+AS
+BEGIN
+	
+END
+GO
